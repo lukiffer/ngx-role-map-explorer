478f3dc399315484437f822f581f305fe6d7351d
Luke Fritz <lukefritz@gmail.com>
2024-05-06 21:53:26 -0700

    Update angular packages to latest version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
-// import { HttpClientModule } from '@angular/common/http';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -12,9 +11,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
-// import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
-// import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { AppComponent } from './app.component';
 
@@ -27,9 +24,7 @@ const ANGULAR_MATERIAL_IMPORTS: any[] = [
   MatIconModule,
   MatInputModule,
   MatSelectModule,
-  // MatTabsModule,
   MatToolbarModule,
-  // MatTooltipModule,
 ];
 
 @NgModule({
@@ -40,7 +35,6 @@ const ANGULAR_MATERIAL_IMPORTS: any[] = [
     BrowserAnimationsModule,
     BrowserModule,
     FormsModule,
-    // HttpClientModule,
     ...ANGULAR_MATERIAL_IMPORTS,
   ],
   providers: [],
